4ac9dc8d14c2f19ee8c97f004dc0751c87193455
vlad <vn@kapri.com.ua>
2024-03-27 19:46:16 +0200

    main function

--- src/data_store.py
+++ src/data_store.py
@@ -19,52 +19,15 @@ class DataStore:
 
     def load_stock_raw(self, symbol: str) -> DataFrame:
         path = f"data/raw/{symbol.upper()}.csv"
-        df_raw = spark.read                 \
-            .option("header", True)     \
-            .csv(path, schema=self.schema)   \
+        df_raw = self.spark.read                 \
+            .option("header", True)         \
+            .csv(path, schema=self.schema)  \
         
-#        rename columns -> lower_snake_case
-
-# 1 - alias       
-#        df_cleaned = df_raw.select(
-#            df_raw['Date'].cast(DateType()).alias('date'),
-#            df_raw['Open'].cast(DoubleType()).alias('open'),
-#            df_raw['Close'].cast(DoubleType()).alias('close'),
-#            df_raw['High'].cast(DoubleType()).alias('high'),
-#            df_raw['Low'].cast(DoubleType()).alias('low')
-#        )
-               
-# 2 - alias + to_snake_case
-#        df_cleaned = df_raw.select(        
-#            df_raw['Date'].cast(DateType()).alias(to_snake_case(df_raw.columns[0])),
-#            df_raw['Open'].cast(DoubleType()).alias(to_snake_case(df_raw.columns[1])),
-#            df_raw['Close'].cast(DoubleType()).alias(to_snake_case(df_raw.columns[2])),
-#            df_raw['High'].cast(DoubleType()).alias(to_snake_case(df_raw.columns[3])),
-#            df_raw['Low'].cast(DoubleType()).alias(to_snake_case(df_raw.columns[4]))            
-#        )
-        
-# 3 - withColumnRenamed
-        df_cleaned = df_raw.select(        
-            df_raw['Date'].cast(DateType()),
-            df_raw['Open'].cast(DoubleType()),
-            df_raw['Close'].cast(DoubleType()),
-            df_raw['High'].cast(DoubleType()),
-            df_raw['Low'].cast(DoubleType())
-        )
-
-        df_cleaned = all_columns_to_snake_case(df_cleaned)
+#       rename columns -> lower_snake_case
+        df_cleaned = all_columns_to_snake_case(df_raw)
 
         return df_cleaned
     
 
 
 # =======================================================
-spark = SparkSession        \
-    .builder                \
-    .appName("data-store")    \
-    .master("local[*]")     \
-    .getOrCreate()
-
-data_store = DataStore(spark)
-df = data_store.load_stock_raw("aapl")
-df.show()
--- src/main.py
+++ src/main.py
@@ -4,3 +4,17 @@ from pyspark.sql.functions import *
 from data_store import DataStore
 
 
+def test_data_store() -> None:
+    spark = SparkSession        \
+        .builder                \
+        .appName("data-store")    \
+        .master("local[*]")     \
+        .getOrCreate()
+
+    data_store = DataStore(spark)
+    df = data_store.load_stock_raw("aapl")
+    df.show()
+
+
+#if __name__ == "__main__":
+#    test_data_store()
